4c6635c5ed8689bbcada31c67487af6c0750c003
teste <teste@gmail.com>
2025-04-24 08:12:08 -0300

    Add controllers

---
+++ OrdemServico/Controllers/OrdensServicoController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OrdemServico.Models;
+
+namespace OrdemServico.Controllers
+{
+    public class OrdensServicoController : Controller
+    {
+        private readonly DBSenaiTechContext _context;
+
+        public OrdensServicoController(DBSenaiTechContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OrdensServico
+        public async Task<IActionResult> Index()
+        {
+            var dBSenaiTechContext = _context.OrdensServicos.Include(o => o.IdClienteNavigation).Include(o => o.IdDefeitoNavigation).Include(o => o.IdEquipamentoNavigation).Include(o => o.IdTecnicoNavigation);
+            return View(await dBSenaiTechContext.ToListAsync());
+        }
+
+        // GET: OrdensServico/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.OrdensServicos == null)
+            {
+                return NotFound();
+            }
+
+            var ordensServico = await _context.OrdensServicos
+                .Include(o => o.IdClienteNavigation)
+                .Include(o => o.IdDefeitoNavigation)
+                .Include(o => o.IdEquipamentoNavigation)
+                .Include(o => o.IdTecnicoNavigation)
+                .FirstOrDefaultAsync(m => m.IdOrdemServico == id);
+            if (ordensServico == null)
+            {
+                return NotFound();
+            }
+
+            return View(ordensServico);
+        }
+
+        // GET: OrdensServico/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "IdCliente");
+            ViewData["IdDefeito"] = new SelectList(_context.Defeitos, "IdDefeito", "IdDefeito");
+            ViewData["IdEquipamento"] = new SelectList(_context.Equipamentos, "IdEquipamento", "IdEquipamento");
+            ViewData["IdTecnico"] = new SelectList(_context.Tecnicos, "IdTecnico", "IdTecnico");
+            return View();
+        }
+
+        // POST: OrdensServico/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdOrdemServico,DataOrdemServico,Servico,ValorTotal,StatusServico,IdEquipamento,IdCliente,IdDefeito,IdTecnico")] OrdensServico ordensServico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ordensServico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "IdCliente", ordensServico.IdCliente);
+            ViewData["IdDefeito"] = new SelectList(_context.Defeitos, "IdDefeito", "IdDefeito", ordensServico.IdDefeito);
+            ViewData["IdEquipamento"] = new SelectList(_context.Equipamentos, "IdEquipamento", "IdEquipamento", ordensServico.IdEquipamento);
+            ViewData["IdTecnico"] = new SelectList(_context.Tecnicos, "IdTecnico", "IdTecnico", ordensServico.IdTecnico);
+            return View(ordensServico);
+        }
+
+        // GET: OrdensServico/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.OrdensServicos == null)
+            {
+                return NotFound();
+            }
+
+            var ordensServico = await _context.OrdensServicos.FindAsync(id);
+            if (ordensServico == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "IdCliente", ordensServico.IdCliente);
+            ViewData["IdDefeito"] = new SelectList(_context.Defeitos, "IdDefeito", "IdDefeito", ordensServico.IdDefeito);
+            ViewData["IdEquipamento"] = new SelectList(_context.Equipamentos, "IdEquipamento", "IdEquipamento", ordensServico.IdEquipamento);
+            ViewData["IdTecnico"] = new SelectList(_context.Tecnicos, "IdTecnico", "IdTecnico", ordensServico.IdTecnico);
+            return View(ordensServico);
+        }
+
+        // POST: OrdensServico/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdOrdemServico,DataOrdemServico,Servico,ValorTotal,StatusServico,IdEquipamento,IdCliente,IdDefeito,IdTecnico")] OrdensServico ordensServico)
+        {
+            if (id != ordensServico.IdOrdemServico)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ordensServico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrdensServicoExists(ordensServico.IdOrdemServico))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "IdCliente", ordensServico.IdCliente);
+            ViewData["IdDefeito"] = new SelectList(_context.Defeitos, "IdDefeito", "IdDefeito", ordensServico.IdDefeito);
+            ViewData["IdEquipamento"] = new SelectList(_context.Equipamentos, "IdEquipamento", "IdEquipamento", ordensServico.IdEquipamento);
+            ViewData["IdTecnico"] = new SelectList(_context.Tecnicos, "IdTecnico", "IdTecnico", ordensServico.IdTecnico);
+            return View(ordensServico);
+        }
+
+        // GET: OrdensServico/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.OrdensServicos == null)
+            {
+                return NotFound();
+            }
+
+            var ordensServico = await _context.OrdensServicos
+                .Include(o => o.IdClienteNavigation)
+                .Include(o => o.IdDefeitoNavigation)
+                .Include(o => o.IdEquipamentoNavigation)
+                .Include(o => o.IdTecnicoNavigation)
+                .FirstOrDefaultAsync(m => m.IdOrdemServico == id);
+            if (ordensServico == null)
+            {
+                return NotFound();
+            }
+
+            return View(ordensServico);
+        }
+
+        // POST: OrdensServico/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.OrdensServicos == null)
+            {
+                return Problem("Entity set 'DBSenaiTechContext.OrdensServicos'  is null.");
+            }
+            var ordensServico = await _context.OrdensServicos.FindAsync(id);
+            if (ordensServico != null)
+            {
+                _context.OrdensServicos.Remove(ordensServico);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrdensServicoExists(int id)
+        {
+          return (_context.OrdensServicos?.Any(e => e.IdOrdemServico == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ OrdemServico/Controllers/TecnicoController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OrdemServico.Models;
+
+namespace OrdemServico.Controllers
+{
+    public class TecnicoController : Controller
+    {
+        private readonly DBSenaiTechContext _context;
+
+        public TecnicoController(DBSenaiTechContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tecnico
+        public async Task<IActionResult> Index()
+        {
+              return _context.Tecnicos != null ? 
+                          View(await _context.Tecnicos.ToListAsync()) :
+                          Problem("Entity set 'DBSenaiTechContext.Tecnicos'  is null.");
+        }
+
+        // GET: Tecnico/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Tecnicos == null)
+            {
+                return NotFound();
+            }
+
+            var tecnico = await _context.Tecnicos
+                .FirstOrDefaultAsync(m => m.IdTecnico == id);
+            if (tecnico == null)
+            {
+                return NotFound();
+            }
+
+            return View(tecnico);
+        }
+
+        // GET: Tecnico/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tecnico/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdTecnico,NomeTecnico")] Tecnico tecnico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tecnico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tecnico);
+        }
+
+        // GET: Tecnico/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Tecnicos == null)
+            {
+                return NotFound();
+            }
+
+            var tecnico = await _context.Tecnicos.FindAsync(id);
+            if (tecnico == null)
+            {
+                return NotFound();
+            }
+            return View(tecnico);
+        }
+
+        // POST: Tecnico/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdTecnico,NomeTecnico")] Tecnico tecnico)
+        {
+            if (id != tecnico.IdTecnico)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tecnico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TecnicoExists(tecnico.IdTecnico))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tecnico);
+        }
+
+        // GET: Tecnico/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Tecnicos == null)
+            {
+                return NotFound();
+            }
+
+            var tecnico = await _context.Tecnicos
+                .FirstOrDefaultAsync(m => m.IdTecnico == id);
+            if (tecnico == null)
+            {
+                return NotFound();
+            }
+
+            return View(tecnico);
+        }
+
+        // POST: Tecnico/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Tecnicos == null)
+            {
+                return Problem("Entity set 'DBSenaiTechContext.Tecnicos'  is null.");
+            }
+            var tecnico = await _context.Tecnicos.FindAsync(id);
+            if (tecnico != null)
+            {
+                _context.Tecnicos.Remove(tecnico);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TecnicoExists(int id)
+        {
+          return (_context.Tecnicos?.Any(e => e.IdTecnico == id)).GetValueOrDefault();
+        }
+    }
+}
