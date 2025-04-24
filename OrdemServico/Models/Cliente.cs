440f599d913cfa35a93e9b128dd384bb6245d0f1
teste <teste@gmail.com>
2025-04-24 08:15:56 -0300

    Remve controller and started refactor of models

--- OrdemServico/Models/Cliente.cs
+++ OrdemServico/Models/Cliente.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace OrdemServico.Models
 {
     public partial class Cliente
     {
+
         public Cliente()
         {
             OrdensServicos = new HashSet<OrdensServico>();
         }
-
+        [Key]
         public int IdCliente { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatória.")]
+        [StringLength(100, ErrorMessage = "O nome não pode ter mais que 100 caracteres.")]
         public string? NomeCliente { get; set; }
 
         public virtual ICollection<OrdensServico> OrdensServicos { get; set; }
